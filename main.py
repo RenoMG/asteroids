f084df070a2ecc81670d8b6debce3c6131442256
RenoMG <reno@renos.world>
2026-01-12 19:49:17 -0800

    create FPS system

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    fps_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,7 +20,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        dt = fps_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
