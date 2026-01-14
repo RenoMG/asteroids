5690d271ca23eaabd5b646863fc709c942403cbf
RenoMG <reno@renos.world>
2026-01-13 23:11:15 -0800

    add asteroid collision

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,9 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # must override
         pass
+
+    def collides_with(self, other):
+        whole_radius = self.radius + other.radius
+        if self.position.distance_to(other.position) and other.position.distance_to(self.position) <= whole_radius:
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import pygame
+import pygame, sys
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -38,6 +38,11 @@ def main():
                 return
         screen.fill("black")
         updatable.update(dt)
+        for item in asteroids:
+            if item.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for item in drawable:
             item.draw(screen)
         pygame.display.flip()
